67782a783e999de04e7423bcd8f95790798f8a6e
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-01-30 08:33:11 +0700

    Update file

--- extract_skeleton.py
+++ extract_skeleton.py
@@ -13,15 +13,41 @@ Outputs:
 
 
 def main():
-    # Read binary image
-    binary_img_path = "test_binary_img.png"
-    img = cv2.imread(binary_img_path, cv2.IMREAD_GRAYSCALE)
+    panicles = ["image_cornflower/1C8-6-1.png",
+                "image_cornflower/1C8-6-2.png",
+                "image_cornflower/1C8-6-3.png",
+                "image_cornflower/1C8-7-1.png",
+                "image_cornflower/1C8-7-2.png",
+                "image_cornflower/1C8-7-3.png",
+                "image_cornflower/1C8-8-1.png",
+                "image_cornflower/1C8-8-2.png",
+                "image_cornflower/1C8-8-3.png",
+                "image_cornflower/1C8-9-1.png",
+                "image_cornflower/1C8-9-2.png",
+                "image_cornflower/1C8-9-3.png",
+                "image_cornflower/1C8-10-1.png",
+                "image_cornflower/1C8-10-2.png",
+                "image_cornflower/1C8-10-3.png",
+                "image_cornflower/1C8-11-1.png",
+                "image_cornflower/1C8-11-2.png",
+                "image_cornflower/1C8-11-3.png",
+                "image_cornflower/1C8-12-1.png",
+                "image_cornflower/1C8-12-2.png",
+                "image_cornflower/1C8-12-3.png"]
+    
+    for path in panicles:
+    
+        # Read binary image
+        # binary_img_path = "test_binary_img.png"
+        binary_img_path = path
+        img = cv2.imread(binary_img_path, cv2.IMREAD_GRAYSCALE)
 
-    # Extract Skeleton
-    skeleton = skeletonize(img, method="zhang").astype(np.uint8) * 255
+        # Extract Skeleton
+        skeleton = skeletonize(img, method="zhang").astype(np.uint8) * 255
 
-    # Save file
-    cv2.imwrite(f"skeleton_dataset/{binary_img_path}", skeleton)
+        # Save file
+        # cv2.imwrite(f"skeleton_dataset/{binary_img_path}", skeleton)
+        cv2.imwrite(f"skeleton_dataset/{binary_img_path[17:]}", skeleton)
 
 
 if __name__ == "__main__":
