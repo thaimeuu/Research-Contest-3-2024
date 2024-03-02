667b71875e51ef07174daff5646c2c5288a330c8
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-03-02 20:27:54 +0700

    update

--- label_dataset.py
+++ label_dataset.py
@@ -1,3 +1,4 @@
+import os
 from vertices_coordinates import find_vertices
 import cv2
 
@@ -16,37 +17,45 @@ Instructions:
 """
 
 def main():
-    # Read grayscale img
-    path = r"grayscale_dataset/1C8-10-2.JPG"
-    img = cv2.imread(path)
+    folder_path = "grayscale_dataset"
+    file_names = os.listdir(folder_path)
+    for file_name in file_names:
+        # Read grayscale img
+        path = folder_path + "/" + file_name
+        img = cv2.imread(path)
 
-    xml_file_path = f"Jouannic_xml/{path[18:-4]}.ricepr"
+        xml_file_path = f"Jouannic_xml/{file_name[:-4]}.ricepr"
+        print(f"===\nExtracting junctions from {xml_file_path}\n===")
 
-    # lists of junctions' coordinate
-    generating = find_vertices("Generating", xml_file_path)
-    primary = find_vertices("Primary", xml_file_path)
-    secondary = find_vertices("Seconday", xml_file_path)  # not typo
-    tertiary = find_vertices("Tertiary", xml_file_path)
-    quaternary = find_vertices("Quaternary", xml_file_path)
-    terminal = find_vertices("End", xml_file_path)
+        # lists of junctions' coordinate
+        generating = find_vertices("Generating", xml_file_path)
+        primary = find_vertices("Primary", xml_file_path)
+        secondary = find_vertices("Seconday", xml_file_path)  # not typo
+        tertiary = find_vertices("Tertiary", xml_file_path)
+        quaternary = find_vertices("Quaternary", xml_file_path)
+        # terminal = find_vertices("End", xml_file_path)
 
-    # Add junctions to grayscale img
-    for a, b in generating:
-        cv2.circle(img, (a, b), 10, (0, 255, 255), cv2.FILLED)
-    for a, b in primary:
-        cv2.circle(img, (a, b), 10, (255, 255, 255), cv2.FILLED)
-    for a, b in secondary:
-        cv2.circle(img, (a, b), 10, (255, 0, 0), cv2.FILLED)
-    for a, b in tertiary:
-        cv2.circle(img, (a, b), 10, (0, 255, 0), cv2.FILLED)
-    for a, b in quaternary:
-        cv2.circle(img, (a, b), 10, (255, 255, 0), cv2.FILLED)
-    for a, b in terminal:
-        cv2.circle(img, (a, b), 10, (0, 0, 255), cv2.FILLED)
+        # Add junctions to grayscale img
+        for a, b in generating:
+            cv2.circle(img, (a, b), 8, (0, 255, 255), cv2.FILLED)
+        for a, b in primary:
+            cv2.circle(img, (a, b), 8, (255, 255, 255), cv2.FILLED)
+        for a, b in secondary:
+            cv2.circle(img, (a, b), 8, (255, 0, 0), cv2.FILLED)
+        for a, b in tertiary:
+            cv2.circle(img, (a, b), 8, (0, 255, 0), cv2.FILLED)
+        for a, b in quaternary:
+            cv2.circle(img, (a, b), 8, (255, 255, 0), cv2.FILLED)
+        # for a, b in terminal:
+        #     cv2.circle(img, (a, b), 8, (0, 0, 255), cv2.FILLED)
 
-    # Save file
-    save_path = rf"labelled_dataset/{path[18:]}"
-    cv2.imwrite(save_path, img)
+        # Save file
+        save_path = rf"labelled_dataset/{file_name}"
+        cv2.imwrite(save_path, img)
+        
+        print(f"Successfully created {save_path}\n")
+        
+        # break
 
 
 if __name__ == "__main__":
