6ea044fd1e6139b5b5001b0c78e0e4c3e7fd06e5
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-03-09 20:36:33 +0700

    update

--- f1-score-by-thai.py
+++ f1-score-by-thai.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import cv2
-from combine_pred_true import combine_pred_true
+from combine_pred_true import combine_pred_true, visualize
 
 """
 Method:
@@ -12,7 +12,7 @@ Evaluate:
     - If a predicted junction is the same as 1 out of 25 true-junction pixels, count as true positives 
 """
 
-def f1_by_thai(y_true_25: np.array, y_true_1: np.array, y_pred: np.array, return_precision=True, return_recall=True) -> float:
+def f1_by_thai(y_true_25: np.array, y_true_1: np.array, y_pred: np.array) -> float:
     """
     ultimate f1-score by thai ^_^
 
@@ -51,9 +51,29 @@ def f1_by_thai(y_true_25: np.array, y_true_1: np.array, y_pred: np.array, return
 
     f1_score = 2 * precision * recall / (precision + recall)
 
-    return f1_score if not (return_precision and return_recall) else [f1_score, precision, recall] if (return_precision and return_recall) else [f1_score, precision] if (return_precision and not return_recall) else [f1_score, recall]
+    return [f1_score, precision, recall]
 
 
+def f1_main_axis(y_true_25: np.array, y_true_1: np.array, y_pred: np.array) -> float:
+    true_25, true_1, pred = np.copy(y_true_25), np.copy(y_true_1), np.copy(y_pred)
+    
+    junction_true_25 = np.argwhere(true_25 > 0)
+    
+    lower_bound = np.min(junction_true_25, axis=0)[0]
+    upper_bound = np.max(junction_true_25, axis=0)[0]
+    left_bound = np.min(junction_true_25, axis=0)[1]
+    right_bound = np.max(junction_true_25, axis=0)[1]
+    
+    y_pred[:lower_bound - 1, :] = 0  # -1 for loose bound
+    y_pred[upper_bound + 1 + 1:, :] = 0  # + 1 for loose bound
+    y_pred[:, :left_bound - 1] = 0  # -1 for loose bound
+    y_pred[:, right_bound + 1 + 1:] = 0  # + 1 for loose bound
+    
+    f1_score, precision, recall = f1_by_thai(y_true_25, y_true_1, y_pred)
+    
+    return [f1_score, precision, recall]
+
+    
 if __name__ == "__main__":
     # Dataset 1
     # folder_name = "y_pred/crossing-number/Zhang-Suen/RUC-Net"
@@ -78,26 +98,58 @@ if __name__ == "__main__":
     # print(min(f1_record), max(f1_record), np.mean(f1_record))
     
     # Dataset 2
-    folder_name = "dataset_2_y_pred/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/Zhang-Suen"
-    file_names = os.listdir(folder_name)
+    # folder_name = "dataset_2_y_pred/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/Zhang-Suen"
+    # file_names = os.listdir(folder_name)
+    # f1_record = []
+    # for file_name in file_names:
+    #     print(f"\n{file_name}")
+    #     y_true_25 = cv2.imread(f"dataset_2_y_true/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/y_true_25/{file_name}", cv2.IMREAD_GRAYSCALE)
+    #     y_true_1 = cv2.imread(f"dataset_2_y_true/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/y_true_1/{file_name}", cv2.IMREAD_GRAYSCALE)
+    #     # y_pred = cv2.imread(f"y_pred/crossing-number/gradient-based-optimization/RUC-Net/{file_name}", cv2.IMREAD_GRAYSCALE)
+    #     y_pred = cv2.imread(f"dataset_2_y_pred/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/Zhang-Suen/{file_name}", cv2.IMREAD_GRAYSCALE)
+    #     f1_score, precision, recall = f1_by_thai(y_true_25, y_true_1, y_pred)
+    #     f1_record.append(f1_score)
+    #     print(f1_score, precision, recall)
+        
+    #     file_path = "dataset_2_F1-record.txt"
+    #     with open(file_path, 'a') as f:
+    #         f.write(f"{file_name}: F1 = {f1_score}\n")
+        
+    #     # combine_pred_true(y_true_25, y_pred, visualization=True)
+        
+    #     # break
+    
+    # print(min(f1_record), max(f1_record), np.mean(f1_record))
+    
+    
+    # f1-score for dataset 1 main axis
+    folder_path = "dataset_1_y_true_main_axis/crossing_number/y_true_1"
+    file_names = os.listdir(folder_path)
     f1_record = []
+    
     for file_name in file_names:
-        print(f"\n{file_name}")
-        y_true_25 = cv2.imread(f"dataset_2_y_true/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/y_true_25/{file_name}", cv2.IMREAD_GRAYSCALE)
-        y_true_1 = cv2.imread(f"dataset_2_y_true/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/y_true_1/{file_name}", cv2.IMREAD_GRAYSCALE)
-        # y_pred = cv2.imread(f"y_pred/crossing-number/gradient-based-optimization/RUC-Net/{file_name}", cv2.IMREAD_GRAYSCALE)
-        y_pred = cv2.imread(f"dataset_2_y_pred/.Asian-African panel_CIAT/Asian-African panel_New/crossing_number/Zhang-Suen/{file_name}", cv2.IMREAD_GRAYSCALE)
-        f1_score, precision, recall = f1_by_thai(y_true_25, y_true_1, y_pred)
+        print(f"=====\nEXAMINING {file_name}....\n============")
+        y_true_25 = cv2.imread(f"dataset_1_y_true_main_axis/crossing_number/y_true_25/{file_name}", cv2.IMREAD_GRAYSCALE)
+        y_true_1 = cv2.imread(f"dataset_1_y_true_main_axis/crossing_number/y_true_1/{file_name}", cv2.IMREAD_GRAYSCALE)
+        y_pred = cv2.imread(f"dataset_1_y_pred/crossing-number/Zhang-Suen/RUC-Net/{file_name}", cv2.IMREAD_GRAYSCALE)
+        
+        f1_score, precision, recall = f1_main_axis(y_true_25, y_true_1, y_pred)
         f1_record.append(f1_score)
         print(f1_score, precision, recall)
         
-        file_path = "dataset_2_F1-record.txt"
-        with open(file_path, 'a') as f:
-            f.write(f"{file_name}: F1 = {f1_score}\n")
+        # file_path = "dataset_1_main_axis_F1-record.txt"
+        # with open(file_path, 'a') as f:
+        #     f.write(f"{file_name}: F1 = {f1_score}\n")
         
-        # combine_pred_true(y_true_25, y_pred, visualization=True)
+        original_grayscale_img = cv2.imread(f"dataset_1_grayscale/{file_name[:-4]}.JPG")
+        visualize(original_grayscale_img, y_true_25, y_pred, f"dataset_1_main_axis/{file_name}")
         
+        print("")
+    
         # break
     
     print(min(f1_record), max(f1_record), np.mean(f1_record))
+    
+    
+    # f1-score for dataset 2 main axis
     
