3a3a270d2e808c798628edce0c92610248a396c5
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-03-12 18:07:43 +0700

    add

--- test.py
+++ test.py
@@ -5,50 +5,38 @@ from sklearn.metrics import f1_score
 from new_coordinate import new_coordinate
 
 
-y_pred = cv2.imread("test-RUC-Net/y_pred_255_1C8-12-3.png", cv2.IMREAD_GRAYSCALE)
-y_true = cv2.imread("test-RUC-Net/y_true_255_1C8-12-3.png", cv2.IMREAD_GRAYSCALE)
+# y_pred_path = "dataset_1_y_pred/crossing-number/gradient-based-optimization/RUC-Net/1C8-9-3.png"
+y_pred_path = "dataset_1_y_pred/crossing-number/Zhang-Suen/RUC-Net/1C8-9-3.png"
+y_pred = cv2.imread(y_pred_path, cv2.IMREAD_GRAYSCALE)
 
-junction_true = np.argwhere(y_true)
-junction_pred = np.argwhere(y_pred)
-print(len(junction_pred))
-print(len(junction_true))
+y_true_path = "dataset_1_y_true_main_axis/crossing_number/y_true_25/1C8-9-3.png"
+y_true = cv2.imread(y_true_path, cv2.IMREAD_GRAYSCALE)
 
-for [x, y] in junction_true:
-    # cv2.circle(y_true, [y, x], 2, 255, -1)  # one junction equals 13 pixels
-    y_true[x-2:x+3, y-2:y+3] = 255  # one junction equals 25 pixels
+junction_true_25 = np.argwhere(y_true > 0)
     
-junction_true = np.argwhere(y_true)
-print(len(junction_true))
+lower_bound = np.min(junction_true_25, axis=0)[0]
+upper_bound = np.max(junction_true_25, axis=0)[0]
+left_bound = np.min(junction_true_25, axis=0)[1]
+right_bound = np.max(junction_true_25, axis=0)[1]
 
-original_image = cv2.imread("grayscale_dataset/1C8-12-3.JPG")
-original_image = cv2.resize(original_image, [256, 256])
+y_pred[:lower_bound - 1, :] = 0  # -1 for loose bound
+y_pred[upper_bound + 1 + 1:, :] = 0  # + 1 for loose bound
+# y_pred[:, :left_bound - 1] = 0  # -1 for loose bound
+y_pred[:, right_bound + 1 + 1:] = 0  # + 1 for loose bound
 
-for x, y in junction_true:
-    original_image[x, y] = (255, 0, 0)
-    
-for x, y in junction_pred:
-    original_image[x, y] = (0, 0, 255)
-    
-# cv2.imshow("original", original_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# cv2.imwrite(f"dataset_1_y_pred_main_axis/zhang-cn/{y_pred_path[-11:]}", y_pred)
 
-cv2.imwrite("test-RUC-Net/original_image_with_big_true_junctions.png", original_image)
-    
-# cv2.imshow("true", y_true)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+white_px = np.argwhere(y_pred > 0)
 
-# cv2.imwrite("test-RUC-Net/Big-junction-true.png", y_true)
+original_grayscale = cv2.imread("dataset_1_grayscale/1C8-9-3.JPG")
+original_grayscale = cv2.resize(original_grayscale, [256, 256])
 
-# junction_pred = tuple(map(tuple, junction_pred))
-# junction_true = tuple(map(tuple, junction_true))
-
-# for junction in junction_pred:
-#     print(f"{junction} is true positive: {junction in junction_true}")
-
-# for i in range(len(junction_pred)):
-#     print(np.linalg.norm(junction_pred[i] - junction_true[i]))
+for x, y in white_px:
+    cv2.circle(original_grayscale, [y, x], 1, (0, 0, 255), -1)
     
     
-# combine_pred_true(y_true, y_pred, visualization=True, save_path="test-RUC-Net/combined.png")
+cv2.imwrite("haha.png", original_grayscale)
+
+cv2.imshow("aks", original_grayscale)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
